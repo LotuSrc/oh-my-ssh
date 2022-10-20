8c20f17325b6812a51c7333f93999f57093cab01
LotuSrc <451415738@qq.com>
2022-10-20 18:00:32 +0800

    feat: support server description field

--- main.py
+++ main.py
@@ -17,40 +17,50 @@ class OmsCli(object):
             [
                 Input("Enter the server name or ip:",),
                 Password("Enter the password:"),
+                Input("Enter the description:"),
             ],
             spacing=0
         )
         result = cli.launch()
-        ret = subprocess.run(["vault", "kv", "patch", "-format=json", "-mount=secret", "test", "{}={}".format(result[0][1], result[1][1])], capture_output=True)
+        ret = subprocess.run(["vault", "kv", "patch", "-format=json", "-mount=secret", "oms/{}".format(result[0][1]), "password={}".format(result[1][1]), "description={}".format(result[2][1])], capture_output=True)
         if not ret.stdout.decode('utf-8'):
             # 数据库为空，需要先put一次
-            ret = subprocess.run(["vault", "kv", "put", "-format=json", "-mount=secret", "test", "{}={}".format(result[0][1], result[1][1])], capture_output=True) 
+            ret = subprocess.run(["vault", "kv", "put", "-format=json", "-mount=secret", "oms/{}".format(result[0][1]), "password={}".format(result[1][1]), "description={}".format(result[2][1])], capture_output=True) 
         console.print("[green]Finish upsert server: [/green][sky_blue3]{}[/sky_blue3]".format(result[0][1]))
 
     def list(self):
-        ret = subprocess.run(["vault", "kv", "get", "-format=json", "-mount=secret", "test"], capture_output=True)
-        data = json.loads(ret.stdout.decode('utf-8'))['data']['data']
+        ret = subprocess.run(["vault", "kv", "list", "-format=json", "secret/oms"], capture_output=True)
+        # data = json.loads(ret.stdout.decode('utf-8'))['data']['data']
+        data = json.loads(ret.stdout.decode('utf-8'))
+
         console.print("[bold medium_purple]All servers are listed:[/bold medium_purple]")
-        for k in data.keys():
-            console.print("[sky_blue3]{}[/sky_blue3]".format(k))
+        # for k in data.keys():
+        for k in data:
+            ret = subprocess.run(["vault", "kv", "get", "-format=json", "-field=description", "-mount=secret", "oms/{}".format(k)], capture_output=True)
+            console.print("[sky_blue3]{}[/sky_blue3]\t{}".format(k, json.loads(ret.stdout.decode('utf-8'))))
 
     def ssh(self):
-        ret = subprocess.run(["vault", "kv", "get", "-format=json", "-mount=secret", "test"], capture_output=True)
-        data = json.loads(ret.stdout.decode('utf-8'))['data']['data']
+        ret = subprocess.run(["vault", "kv", "list", "-format=json", "secret/oms"], capture_output=True)
+        # data = json.loads(ret.stdout.decode('utf-8'))['data']['data']
+        data = json.loads(ret.stdout.decode('utf-8'))
         cli = Bullet(
             prompt="Choose from the servers below:",
-            choices=list(data.keys())
+            # choices=list(data.keys())
+            choices=data
         )
         result = cli.launch()
-        child = pexpect.spawn(f"ssh {result}")
+        child = pexpect.spawn("ssh {}".format(result))
         i = child.expect([".*password.*", ".*continue.*?",
                           pexpect.EOF, pexpect.TIMEOUT])
+
+        ret = subprocess.run(["vault", "kv", "get", "-format=json", "-mount=secret", "oms/{}".format(result)], capture_output=True)
+        data = json.loads(ret.stdout.decode('utf-8'))['data']['data']
         if i == 0:
-            child.sendline("{}\n".format(data[result]))
+            child.sendline("{}\n".format(data['password']))
             child.interact()
         elif i == 1:
             child.sendline("yes\n")
-            child.sendline("{}\n".format(data[result]))
+            child.sendline("{}\n".format(data['password']))
             child.interact()
 
     def scp(self):
